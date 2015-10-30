cf72997482735b779053e8a5e6a8327da171a96b
David Navarro de la Morena <dndlm94@gmail.com>
2015-10-30 14:46:54 +0100

    Actividad B2 Terminada
    Se ha “solucionado” el bucle del código Correr.java de la actividad B y
    se han comentado tanto el problema como la solución y su porqué.

--- Tarea3_David_Jose/src/package_b/Correr.java
+++ Tarea3_David_Jose/src/package_b/Correr.java
@@ -7,63 +7,49 @@ class Puerta {
 }
 
 class LlaveA extends Thread {
-	// Metodo de ejecución del hilo.
 	public void run() {
-		// Este while se queda en bucle hasta que no se cumpla la condicion.
-		// Debido a la estructura, la condicion no deja de cumplirse nunca.
 		while (!Puerta.CerrojoB)
-			// Inicio del While
-			;
-		// Fin del While
-
-		// Esta parte de codigo nunca se ejecuta.
-		Puerta.CerrojoA = true; // Condicion que finaliza el While del hilo
-								// LLaveA
+			Puerta.CerrojoA = true;
 		System.out.println("LlaveA terminando");
 	}
 }
 
 class LlaveB extends Thread {
-	// Metodo de ejecución del hilo.
 	public void run() {
-		// Este while se queda en bucle hasta que no se cumpla la condicion.
-		// Debido a la estructura, la condicion no deja de cumplirse nunca.
 		while (!Puerta.CerrojoA)
-			// Inicio del While
 			;
-		// Fin del While
-
-		// Esta parte de codigo nunca se ejecuta.
-		Puerta.CerrojoB = true; // Condicion que finaliza el While del hilo
-								// LLaveA
+		Puerta.CerrojoB = true;
 		System.out.println("LlaveB terminando");
 	}
 }
 
 public class Correr {
 	public static void main(String[] args) throws InterruptedException {
-		// Inicializa las varibles a false.
 		Puerta.CerrojoA = false;
 		Puerta.CerrojoB = false;
-		// Crea dos nuevos hilo; El hilo LlaveA y el hilo LlaveB.
 		Thread a = new LlaveA();
 		Thread b = new LlaveB();
-		// Se inicializan los hilos.
 		a.start();
 		b.start();
 		System.out.println("Comienzo del hilo principal");
-		// Se espera que terminen los dos hilos.
 		a.join();
 		b.join();
 		System.out.println("Fin del hilo principal");
-		// Fin de ejecución del programa.
 	}
 
-	// Commit 1
+	// Commit 2
 	/*
-	 * En este programa, se crean dos hilos los cuales dependen el uno del otro
-	 * para finalizar (A depende de odigo de B y viceversa). Sin embargo y
-	 * debido a la colocacion del codigo, estos hilos nunca podran llegar a su
-	 * fin de forma "natural".
+	 * Para que el programa pueda alcanzar el fin de su ejecucion, la solucion
+	 * mas simple es la de eliminar el caracter ; situado dentro del While de
+	 * UNO Y SOLO UNO de los dos hilos (LlaveA o LlaveB).
+	 * 
+	 * Al hacer esto lo que conseguimos es que uno de los hilos cambie el estado
+	 * de uno de los cerrojos (CerrojoA o CerrojoB) a true lo que hace que se
+	 * cumpla la condicion del while del otro hilo, quien cambia el estado del
+	 * cerrojo restante y termina su ejecucion y, al haber cambiado el estado
+	 * del cerrojo restante, hace que se cumpla la condicion del while del otro
+	 * hilo y que este tambien alcance el fin de su ejecución
+	 * 
+	 * Al haber finalizado los dos hilos, el hilo principal, tambien finaliza.
 	 */
 }
