c44d58cfd0e6a57c112ae8b78add7e6b945330b6
David Navarro de la Morena <dndlm94@gmail.com>
2015-11-02 09:56:13 +0100

    Actividad 1 Finalizada
    Se ha completado la actividad 1 de uso de semáforos.

--- Tarea3_David_Jose/src/ejercicio_1_orden/Orden.java
+++ Tarea3_David_Jose/src/ejercicio_1_orden/Orden.java
@@ -1,5 +1,50 @@
 package ejercicio_1_orden;
 
-public class Orden {
+import java.util.concurrent.Semaphore;
 
+class CounterThread extends Thread {
+	Semaphore semaforo;
+	String name;
+	boolean isNumber;
+
+	public CounterThread(String name, boolean isNumber, Semaphore semaforo) {
+		super();
+		this.name = name;
+		this.isNumber = isNumber;
+		this.semaforo = semaforo;
+	}
+
+	public void run() {
+		char c;
+
+		try {
+			semaforo.acquire();
+
+			if (isNumber) {
+				for (int i = 0; i < 10; i++) {
+					System.out.println(i);
+				}
+			} else {
+				for (int i = 97; i < 107; i++) {
+					c = (char) i;
+					System.out.println(c);
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		semaforo.release();
+	}
+}
+
+public class Orden {
+	public static void main(String[] args) {
+		Semaphore semaforo = new Semaphore(1);
+		CounterThread thread1 = new CounterThread("thread1", false, semaforo);
+		thread1.setPriority(1);
+		CounterThread thread2 = new CounterThread("thread2", true, semaforo);
+		thread2.setPriority(10);
+		thread1.start();
+		thread2.start();
+	}
 }
