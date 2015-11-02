32dead3a7a5affd21e6a00e371fcc1decd47dd9d
David Navarro de la Morena <dndlm94@gmail.com>
2015-11-02 10:17:14 +0100

    Actividad 2 - Nuevo código
    Se han creado las clases Coche y Puesto y se ha implementado el método
    initialize() encargado de generar de forma aleatoria el número de
    puestos, coches y el tiempo de trabajo de cada coche.
    La generación de los datos anteriores, funciona de forma correcta.

---
+++ Tarea3_David_Jose/src/ejercicio_2_itv/ITV.java
@@ -0,0 +1,58 @@
+package ejercicio_2_itv;
+
+import java.util.ArrayList;
+
+class Coche {
+	private byte id;
+	private byte time;
+
+	Coche(byte id, byte time) {
+		this.id = id;
+		this.time = time;
+	}
+
+	public byte getID() {
+		return id;
+	}
+
+	public byte getTime() {
+		return time;
+	}
+}
+
+class Puesto {
+	private byte id;
+
+}
+
+public class ITV {
+	private static ArrayList<Coche> coches;
+	private static ArrayList<Puesto> puestos;
+
+	public static void main(String[] args) {
+		coches = new ArrayList<Coche>();
+		puestos = new ArrayList<Puesto>();
+
+		initialize();
+	}
+
+	private static void initialize() {
+		byte cochesTmp;
+		byte puestosTmp;
+
+		cochesTmp = (byte) Math.floor(Math.random() * (20 - (50 + 1)) + (50));
+		puestosTmp = (byte) Math.floor(Math.random() * (1 - (5 + 1)) + (5));
+
+		for (byte i = 0; i < cochesTmp; i++) {
+			byte time = (byte) Math.floor(Math.random() * (10 - (100 + 1)) + (100));
+			Coche coche = new Coche((byte) (i + 1), time);
+			coches.add(coche);
+		}
+
+		for (byte i = 0; i < puestosTmp; i++) {
+			Puesto puesto = new Puesto();
+			puestos.add(puesto);
+		}
+		System.out.println("Se han creado " + coches.size() + " coches y hay " + puestos.size() + " puestos.");
+	}
+}
